9d31df3b68e3afad27f2d15b46a447da2837ce1f
Galuh Prahadi Gumelar <40691793+galuhprahadi96@users.noreply.github.com>
2020-12-07 17:18:04 +0700

    clean code

--- task3-folder/src/index.js
+++ task3-folder/src/index.js
@@ -1,9 +1,7 @@
 const route = require("express").Router();
 
-// import route disini
 const user = require("./routes/user");
 
-// buat middle disini
 route.use("/user", user);
 
 module.exports = route;
